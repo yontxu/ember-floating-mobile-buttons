50c904c10bf53f472cb3eaddbba0bd0293d23484
Yon Zubizarreta <yonzubi@gmail.com>
2017-08-21 09:08:42 +0200

    Initial commit of the empty Ember project

--- index.js
+++ index.js
@@ -2,5 +2,8 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-floating-mobile-buttons'
+  name: 'ember-floating-mobile-buttons',
+  included: function(app) {
+    this._super.included(app);
+  }
 };
