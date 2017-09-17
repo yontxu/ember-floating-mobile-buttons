1ec7a269d3ac24116ec7ef902676889391a81aff
Yon Zubizarreta <yonzubi@gmail.com>
2017-09-17 21:18:25 +0200

    Add ability to handle child button state and position

--- addon/components/floating-mobile-buttons.js
+++ addon/components/floating-mobile-buttons.js
@@ -2,11 +2,29 @@ import Ember from 'ember';
 import layout from '../templates/components/floating-mobile-buttons';
 
 export default Ember.Component.extend({
+  layout,
+  tagName: 'ul',
   classNames: ['floating-buttons'],
-  classNameBindings: ['active:active'],
+  classNameBindings: ['active:active', 'bottom:bottom', 'top:top', 'left:left', 'right:right'],
   active: false,
+  childrenActive: false,
+  hasChildren: false,
+  didReceiveAttrs(){
+    this._super(...arguments);
+    if(Ember.typeOf(this.get('bottom')) !== 'undefined'){
+      this.set('bottom', true);
+    }
+  },
   didInsertElement(){
     this._super(...arguments);
     this.set('active', true);
+    if(Ember.$('.floating-child-button').length > 0){
+      this.set('hasChildren', true);
+    }
+  },
+  actions: {
+    toggleChildren(){
+      this.toggleProperty('childrenActive')
+    }
   }
 });
