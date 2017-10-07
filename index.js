965c74f36b9aeffa23bc3d2b64d1227375085bfe
Yon Zubizarreta <yonzubi@gmail.com>
2017-10-07 23:37:54 +0200

    Upgrade Ember Cli to version 2.15.1

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-/* jshint node: true */
+/* eslint-env node */
 'use strict';
 
 module.exports = {
-  name: 'ember-floating-mobile-buttons',
-  included: function(app) {
-    this._super.included(app);
-  }
+  name: 'ember-floating-mobile-buttons'
 };
